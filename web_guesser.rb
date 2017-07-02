33f2f522144777f3a9970fa9df3748871db18a5d
Chris Eady <chris.eady1@gmail.com>
2017-07-02 14:12:44 -0500

    Add guess limit & cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,28 +1,55 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-set :secret_number, rand(101)
+set :secret_number, rand(100)
+
+@@turns_left = 5
 
 def check_guess(guess)
-  if guess - settings.secret_number > 5
-    "Way too high!"
+  if guess.nil?
+    ['Guess a number!', 'white']
+  elsif guess - settings.secret_number > 5
+    ['Way too high!', 'red']
   elsif guess > settings.secret_number
-    "Too high!"
+    ['Too high!', 'lightCoral']
   elsif guess - settings.secret_number < -5
-    "Way too low."
+    ['Way too low.', 'red']
   elsif guess < settings.secret_number
-    "Too low."
+    ['Too low.', 'lightCoral']
   elsif guess == settings.secret_number
-    "You guessed it!"
+    @@turns_left = 5
+    settings.secret_number = rand(100)
+    ['You guessed it! Generating new number... Guess again!', 'green']
   else
-    "That's not a number!"
+    ["Error! Are you sure that's a number?", 'yellow']
   end
 end
 
-get '/' do
-  guess = params['guess'].to_i
-  message = check_guess(guess)
-  erb :index, locals: { number: settings.secret_number, guess: guess, message: message
-  }
+def out_of_turns
+  message = "You're out of turns! The SECRET NUMBER was " \
+            "#{settings.secret_number}. Generating new number... " \
+            'Guess again!'
+  background_color = 'white'
+  settings.secret_number = rand(100)
+  [message, background_color]
 end
 
+get '/' do
+  cheat = params['cheat']
+
+  turns_left = @@turns_left
+  if @@turns_left.zero?
+    message, background_color = out_of_turns
+    @@turns_left = 4
+  else
+    guess = (params['guess'] ? params['guess'].to_i : nil)
+    message, background_color = check_guess(guess)
+    @@turns_left -= 1
+  end
+  erb :index, locals: { number: settings.secret_number,
+                        turns: turns_left,
+                        guess: guess,
+                        message: message,
+                        background_color: background_color,
+                        cheat_mode: cheat }
+end
